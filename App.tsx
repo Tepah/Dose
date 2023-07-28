2c1d707c5717bdabec860c7a7ee819fe64845cca
Pete Potipitak <67202848+Tepah@users.noreply.github.com>
2023-07-28 01:25:39 -0700

    starting authentification

--- App.tsx
+++ App.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import Navigator from './src/components/Navigator';
-import { StatusBar } from "react-native";
+import {StatusBar} from 'react-native';
+import {SignInScreen} from './src/components/SignIn';
+import firebaseInit from './src/firebase/config';
 
 function App(): JSX.Element {
-  // const isDarkMode = useColorScheme() === 'dark';
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState<any>(null);
+
+  firebaseInit();
+
   return (
     <NavigationContainer>
       <StatusBar barStyle="light-content" />
-      <Navigator />
+      {!user ? <SignInScreen /> : <Navigator />}
     </NavigationContainer>
   );
 }
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,16 +1,18 @@
 import firebase from '@react-native-firebase/app';
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyAcf77XokuJN_yvmhn53Ll2m_GOfXSLkzA',
-  authDomain: 'dose-1a0df.firebaseapp.com',
-  projectId: 'dose-1a0df',
-  storageBucket: 'dose-1a0df.appspot.com',
-  messagingSenderId: '603937311096',
-  appId: '1:603937311096:ios:f66685a665bb39e699a08d',
+const firebaseInit = async () => {
+  const firebaseConfig = {
+    apiKey: 'AIzaSyAcf77XokuJN_yvmhn53Ll2m_GOfXSLkzA',
+    authDomain: 'dose-1a0df.firebaseapp.com',
+    projectId: 'dose-1a0df',
+    storageBucket: 'dose-1a0df.appspot.com',
+    messagingSenderId: '603937311096',
+    appId: '1:603937311096:ios:f66685a665bb39e699a08d',
+  };
+
+  if (!firebase.apps.length) {
+    await firebase.initializeApp(firebaseConfig);
+    console.log('Firebase Initialized');
+  }
 };
-
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
-
-export default firebase;
+export default firebaseInit;
