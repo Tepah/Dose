1d27ccafaf6f10a85b721fd1f1f4a8e157ba179c
Pete Potipitak <67202848+Tepah@users.noreply.github.com>
2023-08-21 15:11:35 -0700

    Fixed config

--- src/configs/firebase/config.js
+++ src/configs/firebase/config.js
@@ -1,13 +1,14 @@
 import firebase from '@react-native-firebase/app';
+import Config from 'react-native-config';
 
 const firebaseInit = async () => {
   const firebaseConfig = {
-    apiKey: 'AIzaSyAcf77XokuJN_yvmhn53Ll2m_GOfXSLkzA',
-    authDomain: 'dose-1a0df.firebaseapp.com',
-    projectId: 'dose-1a0df',
-    storageBucket: 'dose-1a0df.appspot.com',
-    messagingSenderId: '603937311096',
-    appId: '1:603937311096:ios:f66685a665bb39e699a08d',
+    apiKey: Config.FIREBASE_API_KEY,
+    authDomain: Config.FIREBASE_AUTH_DOMAIN,
+    projectId: Config.FIREBASE_PROJECT_ID,
+    storageBucket: Config.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: Config.FIREBASE_MESSAGING_SENDER_ID,
+    appId: Config.FIREBASE_APP_ID,
   };
 
   if (!firebase.apps.length) {
